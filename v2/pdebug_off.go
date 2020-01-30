4f02a8550b852927275d5bde36e727d09db9f24a
Daisuke Maki <lestrrat+github@gmail.com>
2020-01-30 15:13:53 +0900

    Add brand new code for v2

---
+++ v2/pdebug_off.go
@@ -0,0 +1,41 @@
+// +build !debug,!debug0
+
+package pdebug
+
+import (
+	"context"
+	"io"
+)
+
+const Enabled = false
+
+func WithClock(ctx context.Context, _ Clock) context.Context {
+	return ctx
+}
+
+func WithOutput(ctx context.Context, _ io.Writer) context.Context {
+	return ctx
+}
+
+func WithPrefix(ctx context.Context, _ string) context.Context {
+	return ctx
+}
+
+func WithTimestamp(ctx context.Context, _ bool) context.Context {
+	return ctx
+}
+
+type MarkerGuard struct {
+}
+
+func Marker(_ context.Context, _ string, _ ...interface{}) *MarkerGuard {
+	return nil
+}
+
+func (mg *MarkerGuard) Bind(_ *error) *MarkerGuard {
+	return nil
+}
+
+func (mg *MarkerGuard) End() {}
+
+func Printf(_ context.Context, _ string, _ ...interface{}) {}
