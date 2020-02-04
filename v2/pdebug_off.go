6d223a8d73dd929d521b1136e7866e19b3ef44e9
Daisuke Maki <lestrrat+github@gmail.com>
2020-02-04 10:19:42 +0900

    Provide empty context

--- v2/pdebug_off.go
+++ v2/pdebug_off.go
@@ -9,6 +9,13 @@ import (
 
 const Enabled = false
 
+func Context(ctx context.Context) context.Context {
+	if ctx == nil {
+		return context.Background()
+	}
+	return ctx
+}
+
 func WithClock(ctx context.Context, _ Clock) context.Context {
 	return ctx
 }
